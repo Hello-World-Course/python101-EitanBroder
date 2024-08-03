2da7aeefe12cd062fccfd1ebab61fd38ad2f12b2
USER <eitanbroder@gmail.com>
2024-08-03 21:53:30 +0300

    Merge remote-tracking branch 'origin/assignment_lesson_5' into assignment_lesson_5

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -4,17 +4,17 @@ if len(name) <= 2:
     print('Your name is too short')
     name = None
 else:
-    board_size = int(input(f"{name},please choose board size"))
+    board_size = int(input(f"{name}, please choose board size"))
     if 26 >= board_size <= 0:
-        print(f"{name},you have entered illegal board size")
+        print(f"{name}, you have entered illegal board size")
         board_size = None
     else:
-        number_of_mines = int(input(f"{name}, for board size " f"{board_size},choose number of mines to allocate"))
-        if number_of_mines <= 0 or number_of_mines >= board_size/2:
-            print(f"{name},you have entered illegal number of mines")
+        number_of_mines = int(input(f"{name}, for board size " f"{board_size}, choose number of mines to allocate"))
+        if number_of_mines <= 0 or number_of_mines >= board_size*board_size/2:
+            print(f"{name}, you have entered illegal number of mines")
             number_of_mines = None
         else:
-            print(f"{name},the board size is: " f"{board_size},number of mines is: " f"{number_of_mines}.ENJOY!")
+            print(f"{name}, the board size is: " f"{board_size}, number of mines is: " f"{number_of_mines}. ENJOY!")
 
 
 
