285cae8daa8ae7b75285cfbf5a8fe668c22c97ff
USER <eitanbroder@gmail.com>
2024-08-11 20:48:55 +0300

    ok

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -14,23 +14,25 @@ def register_user():
     name = input("Hello,whats your name")
     if not is_name_valid(name):
         print('Your name is too short')
-        name = None
-    else:
-        board_size = int(input(f"{name}, please choose board size"))
-        if not is_board_size_valid(board_size):
-            print(f"{name}, you have entered illegal board size")
-            board_size = None
-        else:
-            number_of_mines = int(input(f"{name}, for board size " f"{board_size}, choose number of mines to allocate"))
-            if not is_number_of_mines_valid(board_size, number_of_mines):
-                print(f"{name}, you have entered illegal number of mines")
-                number_of_mines = None
-            else:
-                print(f"{name}, the board size is: " f"{board_size}, number of mines is: " f"{number_of_mines}. ENJOY!")
+        return None, None, None
 
+    board_size = int(input(f"{name}, please choose board size"))
+    if not is_board_size_valid(board_size):
+        print(f"{name}, you have entered illegal board size")
+        return None, None, None
 
+    number_of_mines = int(input(f"{name}, for board size " f"{board_size}, choose number of mines to allocate"))
+    if not is_number_of_mines_valid(board_size, number_of_mines):
+        print(f"{name}, you have entered illegal number of mines")
+        number_of_mines = None
+        return None, None, None
 
+    print(f"{name}, the board size is: " f"{board_size}, number of mines is: " f"{number_of_mines}. ENJOY!")
+    return name, board_size, number_of_mines
 
+import project.ui.user_interaction as user_interaction
+real_result = user_interaction.register_user()
+print(real_result)
 
 
 
